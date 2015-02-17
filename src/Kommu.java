0771da339d95d37490b7016a9469ea2949f9c19f
Martin Vieth <martin_123_vieth@hotmail.com>
2015-02-17 16:09:22 +0100

    Joption og inputs

--- src/Kommu.java
+++ src/Kommu.java
@@ -1,4 +1,16 @@
 
+import javax.swing.*;
+
 public class Kommu {
+
+	public void Kommu() {
+		JFrame frame = new JFrame("asd");
+		
+		String IPinput = (String) JOptionPane.showInputDialog(frame, "input IP address");
+		String PortAdrres =(String) JOptionPane.showInputDialog(frame, "input port address");
+		System.out.println("Port address " + PortAdrres);
+		System.out.println("IpAdress " +IPinput);
+		
+	}
 
 }
---
+++ src/RunRun.java
@@ -0,0 +1,9 @@
+
+public class RunRun {
+
+	public static void main(String[] args){
+		Kommu run = new Kommu();
+		
+		run.Kommu();
+	}
+}
