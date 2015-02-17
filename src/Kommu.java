c1f6b582b38ed2662497c2eaf63870be39c43ebb
Martin Vieth <martin_123_vieth@hotmail.com>
2015-02-17 16:36:00 +0100

    ARbejder med socket nu, stjæler lidt her og alt

--- src/Kommu.java
+++ src/Kommu.java
@@ -1,16 +1,47 @@
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 import javax.swing.*;
 
 public class Kommu {
 
 	public void Kommu() {
 		JFrame frame = new JFrame("asd");
-		
+
 		String IPinput = (String) JOptionPane.showInputDialog(frame, "input IP address");
 		String PortAdrres =(String) JOptionPane.showInputDialog(frame, "input port address");
-		System.out.println("Port address " + PortAdrres);
-		System.out.println("IpAdress " +IPinput);
-		
-	}
 
+		Socket s = null;  
+		DataOutputStream outputStream = null;
+		DataInputStream inputstream = null;
+
+		System.out.println("IpAdress " +IPinput);
+		System.out.println("Port address " + PortAdrres);
+
+		Scanner scanner = new Scanner(System.in);
+
+		try {
+			s = new Socket(IPinput,Integer.parseInt(PortAdrres));
+			outputStream = new DataOutputStream(s.getOutputStream());
+			inputstream = new DataInputStream(s.getInputStream());
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host: hostname");
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to: hostname");
+		}
+		while(scanner.hasNext()){
+			String dataInput = scanner.nextLine();
+
+
+
+
+		}
+
+	}
 }
